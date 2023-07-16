317eb07488b7829c84bc7af90c7f1b6448e9cf4e
Boubker Ahbibe <bahbibe@Boubkers-MacBook-Pro.local>
2023-07-16 22:34:23 +0100

    add cpp01_ex00

---
+++ cpp01/Zombie.cpp
@@ -0,0 +1,19 @@
+#include "Zombie.hpp"
+
+Zombie::Zombie(){
+
+};
+Zombie::Zombie(std::string name)
+{
+	this->_name = name;
+};
+
+Zombie::~Zombie()
+{
+	std::cout << this->_name << " Has been destroyed" << std::endl;
+};
+
+void Zombie::announce()
+{
+	std::cout << this->_name << ": BraiiiiiiinnnzzzZ..." << std::endl;
+};
