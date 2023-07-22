535dfc5a5d6169d6a3e9401001c03b4844888d7d
Boubker Ahbibe <bahbibe@Boubkers-MacBook-Pro.local>
2023-07-23 00:04:18 +0100

    add ex03

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+void ft_putstr
+
+int main(const char **rgc, char)
+{
+   int a = 12;
+   int b = 88;
+   swap(a, b);
+     res = res * 16 ;
+     res = res + *str - '0';
+    return 0;
+}
