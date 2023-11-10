746a5683aeb67f6021a3e65056ac96d7f0b5a07c
Boubker Ahbibe <boubkerahbibe@gmail.com>
2023-11-10 07:06:55 +0100

    add 02 main

--- cpp06/ex02/main.cpp
+++ cpp06/ex02/main.cpp
@@ -12,3 +12,32 @@ int main(void)
     delete base;
     return (0);
 }
+
+// class MyClass
+// {
+// private:
+//     int x;
+
+// public:
+//     MyClass(int value) : x(value) {}
+//     int getX() { return x; }
+// };
+
+// class Hack
+// {
+// public:
+//     int x;
+// };
+
+// int main() {
+    
+//   MyClass obj(100);
+//   std::cout << obj.getX() << std::endl;
+  
+//   Hack* hk = reinterpret_cast<Hack*>(&obj);
+//   hk->x = 500;
+  
+//   std::cout << obj.getX() << std::endl; 
+  
+//   return 0;
+// }
